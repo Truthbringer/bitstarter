1d5650f4cbbeb7d041f5f3cd74c03b477c02b92f
Truthbringer <andy.rachels@outlook.com>
2013-07-11 03:34:42 -0400

    Update grader.js

--- grader.js
+++ grader.js
@@ -65,7 +65,17 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL path')
 	.parse(process.argv);
+	
+	 var htmlFile = program.file;
+    	if(program.url){
+	     		rest.get(program.url).on('complete', function(result){
+	       		fs.writeFileSync("url.html", result);
+	     		});
+	     		htmlFile = "url.html";
+    	}
+	
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
